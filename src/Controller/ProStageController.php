8a10a40750e647f6c1f65dbeb6af36af8bca0bda
Steven Soutumier <nevets47150@gmail.com>
2018-12-05 12:02:53 +0100

    Creation route controller

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,16 +4,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage")
      */
     public function index()
     {
-        return $this->render('pro_stage/index.html.twig', [
+        /*return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
-        ]);
+        ]);*/
+        return new Response('<html><body><h1> Bienvenue sur Prostage </h1></body></html>');
+    }
+    /**
+     * @Route("/entreprise",name= "proStageEntreprise")
+     */
+    public function afficherEntreprise()
+    {
+        return new Response('<html><body><h1> Liste des entreprises </h1></body></html>');
+    }
+    /**
+     * @Route("/formation",name="proStageFormation")
+     */
+    public function afficherFormation()
+    {
+        return new Response('<html><body><h1> Liste des formations</h1></body></html>');
+    }
+    /**
+     * @route("/stage/{id}",name="proStage_Stage")
+     */
+    public function afficherStage($id)
+    {
+        return new Response("<html><body><h1>Bienvenue sur le stage $id  </h1></body><html>");
     }
 }
